9eeba3a39411ffdcc9d94a9276a4f53d3a1e3b68
Naveed Jamali <naveedjamali@outlook.com>
2021-10-29 10:48:45 +0500

    Calculations verified.

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,35 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter years of Service: ");
+
         int years = scanner.nextInt();
-        int bonusPercent = 0;
-        if(years<=1){
+        double bonusPercent = 0;
+
+        if (years <= 1) {
 
             bonusPercent = 10;
-        }else if(years >=2 && years <=5)
-        {
+
+        } else if (years >= 2 && years <= 5) {
+
             bonusPercent = 20;
-        }
-        else if (years >= 6 && years <=10)
-        {
+
+        } else if (years >= 6 && years <= 10) {
+
             bonusPercent = 50;
-        }else{
+
+        } else {
+
             bonusPercent = 75;
+
         }
 
-        System.out.print("Enter years of Service: ");
-        int salary = scanner.nextInt();
+        System.out.print("Enter salary: ");
+        double salary = scanner.nextInt();
 
-        double bonus = (bonusPercent/salary)*100;
-        System.out.println("Salary with bonus is "+bonus);
+        double bonus = (salary / 100) * bonusPercent;
+
+        System.out.println("Salary with bonus is " + (salary + bonus));
     }
 }
